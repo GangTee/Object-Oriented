29c888845e2d20e932233f85790e8581a9193fab
ucasstudent <student@mails.ucas.ac.cn>
2018-03-15 13:57:15 +0800

    autocmt: vivado_prj HW_ACT=wav_chk HW_VAL="counter"

--- hardware/sources/ip_catalog/counter.v
+++ hardware/sources/ip_catalog/counter.v
@@ -25,7 +25,7 @@ begin
 	STATE_HALT: ;
 	STATE_RUN: 
 	begin
-		times <= ((times<interval) ? (times+1) : interval);
+		times <= ((times<interval) ? (times+1) : 0);
 		counter <= ((times==interval) ? (counter+1) : counter);
 	end
 	default: 
